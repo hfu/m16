de24aafd44f36d07c64343c3b5f1a12a74d0c69b
Hidenori Fujimura <hfu@users.noreply.github.com>
2019-02-15 20:19:22 -0500

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const path = require('path')
 module.exports = (z, x, y) => {
-  return `${path.dirname(module.filename)}`
+  return `${path.dirname(module.filename)}/2-${x >> (z - 2)}-${y >> (z - 2)}.osm.pbf`
 }
 
---
+++ produce.js
@@ -0,0 +1,7 @@
+const config = require('config')
+const { spawn } = require('child_process')
+const tilebelt = require('@mapbox/tilebelt')
+
+for (x = 0; x < 3; x++) {
+  for (y = 0; y < 3; y++) {
+  
