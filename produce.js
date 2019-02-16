7d360c449d355457ee163e920d8546747e4582a9
Hidenori Fujimura <hfu@users.noreply.github.com>
2019-02-15 20:34:50 -0500

    update

--- produce.js
+++ produce.js
@@ -2,6 +2,34 @@ const config = require('config')
 const { spawn } = require('child_process')
 const tilebelt = require('@mapbox/tilebelt')
 
-for (x = 0; x < 3; x++) {
-  for (y = 0; y < 3; y++) {
-  
+const produce = (z, x, y) => {
+  return new Promise((resolve, result) => {
+    const bbox = tilebelt.tileToBBOX([x, y, 2])
+    const tmpPath = `pbf/part-2-${x}-${y}.osm.pbf`
+    const dstPath = `pbf/2-${x}-${y}.osm.pbf`
+    spawn('osmium', [
+      'extract', '--bbox', bbox.join(','),
+      '--strategy=smart', '--overwrite', '--progress',
+      '--verbose', '--output', tmpPath,
+      config.get('src')
+    ], { stdio: 'inherit' })
+    .on('exit', code => {
+      if (code === 0) {
+        fs.renameSync(tmpPath, dstPath)
+      } else {
+        fs.unlink(tmpPath)
+      }
+      resolve(null)
+    })
+  })
+}
+
+const main = async () => {
+  for (x = 0; x < 4; x++) {
+    for (y = 0; y < 4; y++) {
+      await produce(2, x, y)
+    }
+  }
+}
+
+main()
